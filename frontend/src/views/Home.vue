483a0227bc5380cbafee392b449dbfd8cb9cc6c8
Jordan Burgess <jordanburgess@gmail.com>
2019-03-02 19:48:22 +0000

    Add shop layout

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,24 +1,289 @@
 <template>
+
   <div class="home">
-    <div class="row">
+
+    <main class="xs-all-content-wrapper">
+      <!-- welcome section -->
+      <section
+        class="xs-screen-height xs-welcome-section xs-bg fundpress-welcome-section"
+      >
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="xs-welcome-content fundpress-welcome-content">
+                <div class="xs-welcome-wraper fundpress-welcome-wraper">
+                  <div class="xs-welcome-title fundpress-welcome-title">
+                    <h2 class="color-navy-blue">Exclusive Single Speed Bike</h2>
+                  </div>
+                  <div class="xs-skill-bar-v2" data-percent="65%">
+                    <div class="xs-skill-track">
+                      <p><span class="number-percentage-count">65</span>%</p>
+                    </div>
+                  </div>
+                  <ul
+                    class="xs-list-with-content fundpress-welcome-list-content"
+                  >
+                    <li class='price'>$90 <span>Delivered</span></li>
+                    <li>100 People<span>Goal</span></li>
+                  </ul>
+                  <div class="xs-btn-wraper">
+                    <a href="#" class="xs-btn round-btn navy-blue-btn icon-btn"
+                      ><i class="fa fa-heart"></i>invest Now</a
+                    >
+                    <a href="#" class="xs-btn round-btn blue-btn icon-btn"
+                      ><i class="fa fa-facebook"></i>Share Now</a
+                    >
+                  </div>
+                </div>
+                <!-- .xs-welcome-wraper .fundpress-welcome-wraper END -->
+              </div>
+              <!-- .xs-welcome-content .fundpress-welcome-content END -->
+            </div>
+            <div class="col-lg-6">
+              <div class="xs-welcome-content">
+                <div class="fundpress-animate text-center">
+                  <img src="images/Fixed-Gear-Bike-Track-6-Matte-Red.jpg" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- End welcome section -->
+
+ 
+
+      <!-- popular campaigns -->
+      <section
+        id="popular-campaigns"
+        class="xs-gray-bg waypoint-tigger xs-section-padding"
+      >
+        <div class="container">
+          <div class="xs-section-heading row xs-margin-0">
+            <div class="fundpress-heading-title xs-padding-0 col-xl-9 col-md-9">
+              <h2 class="color-navy-blue">Popular Campaigns</h2>
+              <span
+                class="xs-separetor dashed-separetor fundpress-separetor"
+              ></span>
+              <p>
+                FundPress has built a platform focused on aiding entrepreneurs,
+                startups, and companies raise capital from anyone.
+              </p>
+            </div>
+            <!-- .xs-heading-title .fundpress-heading-title .xs-col-9 END -->
+            <div
+              class="xs-btn-wraper xs-padding-0 col-xl-3 col-md-3 d-flex-center-end"
+            >
+              <a href="#" class="xs-btn round-btn navy-blue-btn"
+                >all Campaigns</a
+              >
+            </div>
+            <!-- .xs-btn-wraper .xs-col-3 .d-flex-center-end END -->
+          </div>
+          <!-- .xs-section-heading .fundpress-section-heading .xs-spilit-container END -->
+          <div class="row">
+            <div class="col-lg-4">
+              <div class="xs-box-shadow fundpress-popular-item xs-bg-white">
+                <div class="fundpress-item-header">
+                  <img src="assets/images/campaigns/campaigns-1.jpg" alt="" />
+                  <div class="xs-item-header-content">
+                    <div class="xs-skill-bar">
+                      <div class="xs-skill-track">
+                        <p>
+                          <span
+                            class="number-percentage-count number-percentage"
+                            data-value="90"
+                            data-animation-duration="3500"
+                            >0</span
+                          >%
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- .xs-item-header .fundpress-item-header END -->
+                <div
+                  class="fundpress-item-content xs-content-padding bg-color-white"
+                >
+                  <ul class="xs-simple-tag fundpress-simple-tag">
+                    <li><a href="">Technology</a></li>
+                  </ul>
+                  <a
+                    href="#"
+                    class="d-block color-navy-blue fundpress-post-title"
+                    >Splash Drone 3 a Fully Waterproof Drone that floats</a
+                  >
+                  <ul class="xs-list-with-content fundpress-list-item-content">
+                    <li>$67,000<span>Pledged</span></li>
+                    <li>
+                      <span
+                        class="number-percentage-count number-percentage"
+                        data-value="90"
+                        data-animation-duration="3500"
+                        >0</span
+                      >% <span>Funded</span>
+                    </li>
+                    <li>3<span>Days to go</span></li>
+                  </ul>
+                  <span
+                    class="xs-separetor border-separetor xs-separetor-v2 fundpress-separetor"
+                  ></span>
+                  <div class="row xs-margin-0">
+                    <div class="full-round fundpress-avatar">
+                      <img src="assets/images/avatar/avatar_1.jpg" alt="" />
+                    </div>
+                    <div class="xs-avatar-title">
+                      <a href="#"><span>By</span>Ema Watson</a>
+                    </div>
+                  </div>
+                </div>
+                <!-- .fundpress-item-content END -->
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="xs-box-shadow fundpress-popular-item xs-bg-white">
+                <div class="fundpress-item-header">
+                  <img src="assets/images/campaigns/campaigns-2.jpg" alt="" />
+                  <div class="xs-item-header-content">
+                    <div class="xs-skill-bar">
+                      <div class="xs-skill-track">
+                        <p>
+                          <span
+                            class="number-percentage-count number-percentage"
+                            data-value="59"
+                            data-animation-duration="3500"
+                            >0</span
+                          >%
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- .xs-item-header .fundpress-item-header END -->
+                <div
+                  class="fundpress-item-content xs-content-padding bg-color-white"
+                >
+                  <ul class="xs-simple-tag fundpress-simple-tag">
+                    <li><a href="">Charity</a></li>
+                  </ul>
+                  <a
+                    href="#"
+                    class="d-block color-navy-blue fundpress-post-title"
+                    >The Read Read: Braille Literacy Tool for the Blind</a
+                  >
+                  <ul class="xs-list-with-content fundpress-list-item-content">
+                    <li>$33,600<span>Pledged</span></li>
+                    <li>
+                      <span
+                        class="number-percentage-count number-percentage"
+                        data-value="59"
+                        data-animation-duration="3500"
+                        >0</span
+                      >% <span>Funded</span>
+                    </li>
+                    <li>10<span>Days to go</span></li>
+                  </ul>
+                  <span
+                    class="xs-separetor border-separetor xs-separetor-v2 fundpress-separetor"
+                  ></span>
+                  <div class="row xs-margin-0">
+                    <div class="full-round fundpress-avatar">
+                      <img src="assets/images/avatar/avatar_2.jpg" alt="" />
+                    </div>
+                    <div class="xs-avatar-title">
+                      <a href="#"><span>By</span>Kene Williamson</a>
+                    </div>
+                  </div>
+                </div>
+                <!-- .fundpress-item-content END -->
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="xs-box-shadow fundpress-popular-item xs-bg-white">
+                <div class="fundpress-item-header">
+                  <img src="assets/images/campaigns/campaigns-3.jpg" alt="" />
+                  <div class="xs-item-header-content">
+                    <div class="xs-skill-bar">
+                      <div class="xs-skill-track">
+                        <p>
+                          <span
+                            class="number-percentage-count number-percentage"
+                            data-value="35"
+                            data-animation-duration="3500"
+                            >0</span
+                          >%
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- .xs-item-header .fundpress-item-header END -->
+                <div
+                  class="fundpress-item-content xs-content-padding bg-color-white"
+                >
+                  <ul class="xs-simple-tag fundpress-simple-tag">
+                    <li><a href="">Funding</a></li>
+                    <li><a href="">Charity</a></li>
+                  </ul>
+                  <a
+                    href="#"
+                    class="d-block color-navy-blue fundpress-post-title"
+                    >BuildOne: $99 3D Printer w/ WiFi and Auto Bed Leveling!</a
+                  >
+                  <ul class="xs-list-with-content fundpress-list-item-content">
+                    <li>$12,760<span>Pledged</span></li>
+                    <li>
+                      <span
+                        class="number-percentage-count number-percentage"
+                        data-value="35"
+                        data-animation-duration="3500"
+                        >0</span
+                      >% <span>Funded</span>
+                    </li>
+                    <li>66<span>Days to go</span></li>
+                  </ul>
+                  <span
+                    class="xs-separetor border-separetor xs-separetor-v2 fundpress-separetor"
+                  ></span>
+                  <div class="row xs-margin-0">
+                    <div class="full-round fundpress-avatar">
+                      <img src="assets/images/avatar/avatar_3.jpg" alt="" />
+                    </div>
+                    <div class="xs-avatar-title">
+                      <a href="#"><span>By</span>Leo Baddabes</a>
+                    </div>
+                  </div>
+                </div>
+                <!-- .fundpress-item-content END -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- End popular campaigns -->
+    <!-- <div class="row">
       <div class="col-sm">
-        <h1>GroupBuy</h1>
-        <p>Welcome to groupbuy. Please see our daily deal below.</p>
+        <h1>Clubtain</h1>
+        <p>Welcome to Clubtain – where you club together to obtain the best prices on goods and services.</p>
       </div>
     </div>
-    <Listing title="Buy this bike" img="@/assets/Fixed-Gear-Bike-Track-6-Matte-Red.jpg"/>
-  </div>
+    <Listing title="Buy this bike" img="@/assets/Fixed-Gear-Bike-Track-6-Matte-Red.jpg"/> -->
+    </main>
+  </div>  
 </template>
 
 <script>
 // @ is an alias to /src
 
-import Listing from '@/components/Listing.vue';
+import Listing from "@/components/Listing.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    Listing,
+    Listing
   },
+  data: {
+    items: [{ message: "Foo" }, { message: "Bar" }]
+  }
 };
 </script>
